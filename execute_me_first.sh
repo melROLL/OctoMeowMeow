168a9a1ca8af575f75c7ce5233119ad0b5ecc7ed
melROLL <60772441+melROLL@users.noreply.github.com>
2022-10-27 10:07:07 +0100

    Update execute_me_first.sh

--- execute_me_first.sh
+++ execute_me_first.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo "==================================================================="
-echo"This scripts install the requierement necessaries to build the image"
-echo "==================================================================="
+echo "======================================================================="
+echo"= This scripts install the requierement necessaries to build the image ="
+echo "======================================================================="
 sudo apt update 
 wait
 sudo apt-get update 
+wait
+sudo chmod +x build_all.sh build_M2U.sh build_M2Z.sh execute_me_first.sh octocitrico.sh octomeow.sh
 wait
 sudo apt-get install virtualbox -y
 wait
@@ -16,6 +18,6 @@ wait
 wait
 ./octomew.sh assets #download all the assets 
 
-echo "========================"
-echo "OctoMeow is ready to run"
-echo "========================"
+echo "============================"
+echo "= OctoMeow is ready to run ="
+echo "============================"
